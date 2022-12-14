b7688c6cedaf4eddae805a1716aa13f60ddb735a
Emily Port <emilylport@gmail.com>
2022-12-14 13:39:16 -0700

    merchant on index page links to merchant show page

--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'merchant index page' do 
-  before :each do 
-  end
+
   describe 'as a visitor when i visit merchants index' do 
     it 'i see a list of merchants by name and can click on link to go to their show page', :vcr do 
-      visit '/merchants' 
+      visit merchants_path 
 
       expect(page).to have_content("List of Merchants:")
       expect(page).to have_content("Schroeder-Jerde")
@@ -13,7 +12,13 @@ RSpec.describe 'merchant index page' do
 
     end
 
-    it 'on merchant show page i see list of items merchant sells' do 
+    it 'on merchant on index page links to merchant show page', :vcr do 
+      visit merchants_path 
+      click_link("Schroeder-Jerde")
+      expect(page).to have_content("Schroeder-Jerde")
+      expect(page).to_not have_content("Klein, Rempel and Jones")
+
+
 
     end
   end
